465f9d8bf14dba3781e5fd82d7a0bb1b04367556
Matt Kangas <kangas@gmail.com>
2016-11-22 23:14:57 -0500

    twiddling

---
+++ docs/Resources.md
@@ -0,0 +1,11 @@
+Resources
+
+## Tools for processing EDGAR data
+
+https://github.com/datasets/edgar
+https://github.com/andrewkittredge/financial_fundamentals
+
+## Node modules
+
+https://www.npmjs.com/package/levelup
+  - wiki: https://github.com/level/levelup/wiki/Modules
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+'use strict'
+
 module.exports = Usul
 
 var debug = require('debug')('usul')
 var EventEmitter = require('events').EventEmitter
 var inherits = require('inherits')
+var parallel = require('run-parallel')
+
+/**
+ * Usul version.
+ */
+// var VERSION = require('./package.json').version
 
 inherits(Usul, EventEmitter)
 
@@ -12,10 +20,17 @@ inherits(Usul, EventEmitter)
  */
 function Usul (opts) {
   var self = this
-  if (!(self instanceof Usul)) return new Usul(opts)
+  if (!(self instanceof Usul)) {
+    return new Usul(opts)
+  }
   EventEmitter.call(self)
 
-  if (!opts) opts = {}
+  if (!opts) {
+    opts = {}
+  }
+
+  self.destroyed = false
+  self.requests = []
 
   debug('new usul')
 
@@ -32,5 +47,48 @@ function Usul (opts) {
  */
 Usul.prototype.destroy = function (cb) {
   if (this.destroyed) throw new Error('client already destroyed')
-  // this._destroy(null, cb)
+  this._destroy(null, cb)
 }
+
+Usul.prototype._destroy = function (err, cb) {
+  var self = this
+  debug('client destroy')
+  self.destroyed = true
+
+  var tasks = self.requests.map(function (request) {
+    return function (cb) {
+      request.destroy(cb)
+    }
+  })
+
+  // if (self._tcpPool) {
+  //   tasks.push(function (cb) {
+  //     self._tcpPool.destroy(cb)
+  //   })
+  // }
+
+  parallel(tasks, cb)
+
+  if (err) self.emit('error', err)
+
+  self.requests = []
+  // self._tcpPool = null
+}
+
+/**
+ * Check if `obj` is a node Readable stream
+ * @param  {*} obj
+ * @return {boolean}
+ */
+// function isReadable (obj) {
+//   return typeof obj === 'object' && obj != null && typeof obj.pipe === 'function'
+// }
+
+/**
+ * Check if `obj` is a W3C `FileList` object
+ * @param  {*} obj
+ * @return {boolean}
+ */
+// function isFileList (obj) {
+//   return typeof FileList !== 'undefined' && obj instanceof FileList
+// }
