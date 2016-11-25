863efe1748180ebb8c63e53c8e78aaba3aebe1bf
Matt Kangas <kangas@gmail.com>
2016-11-25 13:54:20 -0500

    first working remote request method

--- test/node/edgar.js
+++ test/node/edgar.js
@@ -1,9 +1,11 @@
+const process = require('process')
+
 const test = require('tape')
 const Usul = require('../../')
 
-test('edgar.getOpts', function (t) {
+test('edgar.getOpts', (t) => {
   t.plan(2)
-  let client = new Usul({edgar: {hello: 'booya'}})
+  const client = new Usul({edgar: {hello: 'booya'}})
 
   client.on('error', function (err) { t.fail(err) })
   client.on('warning', function (err) { t.fail(err) })
@@ -14,3 +16,24 @@ test('edgar.getOpts', function (t) {
     t.error(err, 'client destroyed')
   })
 })
+
+if (process.env.USUL_TEST_REMOTE) {
+  test('edgar.dailyIndex.fetch', (t) => {
+    t.plan(2)
+    const client = new Usul()
+
+    client.on('error', (err) => { t.fail(err) })
+    client.on('warning', (err) => { t.fail(err) })
+
+    client.edgar.dailyIndex()
+    .then((data) => {
+      const idx = JSON.parse(data)
+      t.equals(idx.directory.name, 'daily-index/')
+    })
+    .catch((e) => { t.fail(e) })
+
+    client.destroy(function (err) {
+      t.error(err, 'client destroyed')
+    })
+  })
+}
